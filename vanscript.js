af7777b8dc7bd9ec2259ec3b7017be9d62026014
Rajdeep Bastakoti <91162286+RBCodewalker@users.noreply.github.com>
2021-12-08 20:28:00 +0100

    Create vanscript.js

---
+++ vanscript.js
@@ -0,0 +1,10 @@
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
+
+document.addEventListener("DOMContentLoaded", getTodos);
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteTodo);
+filterOption.addEventListener("click", filterTodo);
+
