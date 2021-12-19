8d7346ccc6a90204e8715864c769b35bf031b4a0
Rajdeep Bastakoti <91162286+RBCodewalker@users.noreply.github.com>
2021-12-19 23:04:44 +0100

    Update vanscript.js

--- vanscript.js
+++ vanscript.js
@@ -67,8 +67,25 @@ function filter(e)
   const todos = todoList.childNodes;
     todos.forEach(function(todo)
     {
-      
-    }
+      switch (e.target.value) {
+        case "all":
+          todo.style.display = "flex";
+          break;
+        case "completed":
+          if (todo.classList.contains("comp")) {
+            todo.style.display = "flex";
+          } else {
+            todo.style.display = "none";
+          }
+          break;
+        case "uncompleted":
+          if (!todo.classList.contains("comp")) {
+            todo.style.display = "flex";
+          } else {
+            todo.style.display = "none";
+          }
+      }
+    });
 }
 
 function savelocal(e)
